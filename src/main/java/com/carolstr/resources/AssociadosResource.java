44167844c19d0c92cbba1143009fe10bc2c8380e
Carol Trindade <caroltrindade6@gmail.com>
2023-03-11 17:53:39 -0300

    ajuste swagger

--- src/main/java/com/carolstr/resources/AssociadosResource.java
+++ src/main/java/com/carolstr/resources/AssociadosResource.java
@@ -1,9 +1,17 @@
 package com.carolstr.resources;
 
+import com.carolstr.entities.Associado;
 import com.carolstr.exception.PautaInvalidaException;
 import com.carolstr.requests.AssociadoRequest;
+import com.carolstr.responses.AssociadosResponse;
+import com.carolstr.responses.PautaResponse;
+import com.carolstr.responses.PautasResponse;
 import com.carolstr.services.AssociadosService;
 import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
@@ -20,12 +28,19 @@ public class AssociadosResource {
 
     @POST
     @Operation(summary = "Cadastrar associado")
+    @APIResponses({
+            @APIResponse(responseCode = "201", description = "Associado cadastrado com sucesso",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = Associado.class))),
+            @APIResponse(responseCode = "404", description = "Associado invalido")
+    })
     public Response cadastrarAssociado(AssociadoRequest request) throws PautaInvalidaException {
         return Response.status(Response.Status.CREATED).entity(service.cadastrarAssociado(request)).build();
     }
 
     @GET
     @Operation(summary = "Buscar associados cadastrados no sistema")
+    @APIResponse(responseCode = "200", description = "sucesso", content = @Content(mediaType = MediaType.APPLICATION_JSON,
+            schema = @Schema(implementation = AssociadosResponse.class)))
     public Response buscarAssociados(){
         return Response.status(Response.Status.OK).entity(service.buscarAssociadosCadastrados()).build();
     }
@@ -33,6 +48,10 @@ public class AssociadosResource {
     @DELETE
     @Path("/{id}")
     @Operation(summary = "Excluir associado")
+    @APIResponses({
+            @APIResponse(responseCode = "204", description = "sucesso"),
+            @APIResponse(responseCode = "404", description = "Associado invalido")
+    })
     public Response deletarAssociados(@PathParam("id") String id) throws PautaInvalidaException {
         service.deletarAssociado(id);
         return Response.status(Response.Status.NO_CONTENT).build();
--- src/main/java/com/carolstr/resources/PautasResource.java
+++ src/main/java/com/carolstr/resources/PautasResource.java
@@ -1,11 +1,18 @@
 package com.carolstr.resources;
 
+import com.carolstr.entities.Pauta;
 import com.carolstr.entities.PautaStatus;
 import com.carolstr.exception.PautaInvalidaException;
 import com.carolstr.requests.AtualizarPautaRequest;
 import com.carolstr.requests.PautaRequest;
+import com.carolstr.responses.PautaResponse;
+import com.carolstr.responses.PautasResponse;
 import com.carolstr.services.PautasService;
 import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
@@ -22,12 +29,16 @@ public class PautasResource {
 
     @POST
     @Operation(summary = "Criar pauta")
+    @APIResponse(responseCode = "201", description = "Pauta criada com sucesso", content = @Content(mediaType = MediaType.APPLICATION_JSON,
+            schema = @Schema(implementation = Pauta.class)))
     public Response criarPauta(PautaRequest request) {
         return Response.status(Response.Status.CREATED).entity(service.criarPauta(request)).build();
     }
 
     @GET
     @Operation(summary = "Buscar pautas cadastradas no sistema")
+    @APIResponse(responseCode = "200", description = "sucesso", content = @Content(mediaType = MediaType.APPLICATION_JSON,
+            schema = @Schema(implementation = PautasResponse.class)))
     public Response buscarPautasCadastradas(@QueryParam("status") PautaStatus status, @QueryParam("nome") String nome){
         return Response.ok(service.listarPautasCadastradas(status, nome)).build();
     }
@@ -35,6 +46,11 @@ public class PautasResource {
     @GET
     @Path("/{id}")
     @Operation(summary = "Buscar detalhes da pauta por id")
+    @APIResponses({
+            @APIResponse(responseCode = "200", description = "sucesso", content = @Content(mediaType = MediaType.APPLICATION_JSON,
+                    schema = @Schema(implementation = PautaResponse.class))),
+            @APIResponse(responseCode = "404", description = "Pauta invalida")
+    })
     public Response detalhesPauta(@PathParam("id") String id) throws PautaInvalidaException {
         return Response.ok(service.buscarDetalhesPauta(id)).build();
     }
@@ -42,6 +58,10 @@ public class PautasResource {
     @DELETE
     @Path("/{id}")
     @Operation(summary = "deletar pauta por id")
+    @APIResponses({
+            @APIResponse(responseCode = "204", description = "sucesso"),
+            @APIResponse(responseCode = "404", description = "Pauta invalida")
+    })
     public Response deletarPauta(@PathParam("id") String id) throws PautaInvalidaException {
         service.deletarPauta(id);
         return Response.status(Response.Status.NO_CONTENT).build();
@@ -50,6 +70,10 @@ public class PautasResource {
     @PATCH
     @Path("/{id}")
     @Operation(summary = "atualizar pauta por id")
+    @APIResponses({
+            @APIResponse(responseCode = "204", description = "sucesso"),
+            @APIResponse(responseCode = "404", description = "Pauta invalida")
+    })
     public Response atualizarPauta(@PathParam("id") String id, AtualizarPautaRequest request) throws PautaInvalidaException {
         service.atualizarPauta(id, request);
         return Response.status(Response.Status.NO_CONTENT).build();
@@ -58,6 +82,10 @@ public class PautasResource {
     @PATCH
     @Path("/votar-pauta/{id}/{cpf}")
     @Operation(summary = "atualizar pauta por id")
+    @APIResponses({
+            @APIResponse(responseCode = "204", description = "sucesso"),
+            @APIResponse(responseCode = "404", description = "Pauta invalida")
+    })
     public Response votarPauta(@PathParam("id") String id, @PathParam("cpf") String cpf,
                                    @QueryParam("voto") boolean voto) throws PautaInvalidaException {
         service.votarPauta(id, cpf, voto);
